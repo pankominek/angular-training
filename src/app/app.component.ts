6e0cd9b5297885e952f9b47e66487fbf061140ac
Kamil Kominek <kamil.kominek@ttpsc.pl>
2023-05-24 12:22:07 +0200

    Add initial files

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ttpsc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-training-template';
+}
